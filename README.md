bfdd95cb2eb9f4e996898264165fed1ce66de4b5
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2024-06-25 20:45:03 -0400

    added demo link to readme

--- README.md
+++ README.md
@@ -3,27 +3,36 @@
 
 [Link to this project's repository](https://github.com/Saidou25/Team)
 
-[Link to see a demo](https://drive.google.com/file/d/14hXvCwXkBIykzCq4sz9FhRC-AfK7dJlY/view)
+[View a quick demo](https://drive.google.com/file/d/1NpSii_hqOYo4JwmBGBhkXMlcY5ffgp0w/view?usp=sharing)
 
 ## Table of Contents
 - [Description](#description)
-- [Resources](#resources)
+- [Technology](#technology)
+- [Installation](#installation)
 - [Visuals](#visuals)
 
 ## Description
 
-"Team" is an application that generates a webpage displaying cards of team members.
+"Team" is a node.js application that generates a webpage displaying cards of team members.
 Information for each employee are entered via Vscode's Terminal.
 
+## Technology
 
-## Resources
-This project was created using mostly node and inquirer.
+- `node.js`
+- `inquirer`
+- `jest`
 
+## Installation
+
+. Fork the repository
+. Open live server
+. In the cli type node index.js
+. Answer the prompts
 
 
 ## Visuals
 
-This is a screenshot of what is presented to the user when team members's cards have been generated :
+This is a screenshot of what is presented to the user when all prompts have been answered. Team members's cards have been generated :
 
 
 ![Website Sample](./Images/team.html.png)
--- index.js
+++ index.js
@@ -1,135 +1,127 @@
-const inquirer = require('inquirer');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const fs = require("fs");
 const prompt = inquirer.createPromptModule();
 
 const team = [];
 
 const managerInput = () =>
+  prompt([
+    {
+      type: "input",
+      message: "What is the team managers name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the team manager'id?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the team manager's email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the team manager's office number?",
+      name: "officeNumber",
+    },
+  ])
+    .then(({ name, id, email, officeNumber }) => {
+      const newManager = new Manager(name, id, email, officeNumber);
 
-    prompt([
-        {
-            type: "input",
-            message: "What is the team managers name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "Shat is the team manager'id?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is the team manager's email?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "What is the team manager's office number?",
-            name: "officeNumber",
-        },
+      team.push(newManager);
 
-    ]).then(({ name, id, email, officeNumber }) => {
-        const newManager = new Manager(name, id, email, officeNumber);
-
-        team.push(newManager);
-
-        lastQuest();
-
+      lastQuest();
     })
-        .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 managerInput();
 
 const internInput = () =>
-    prompt([
-        {
-            type: "input",
-            message: "What is your intern's name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is your intern's id?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is your intern's email?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "What is your intern's school?",
-            name: "school",
-        },
+  prompt([
+    {
+      type: "input",
+      message: "What is your intern's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your intern's id?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your intern's email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your intern's school?",
+      name: "school",
+    },
+  ])
+    .then(({ name, id, email, school }) => {
+      const newIntern = new Intern(name, id, email, school);
 
-    ]).then(({ name, id, email, school }) => {
-        const newIntern = new Intern(name, id, email, school);
+      team.push(newIntern);
 
-        team.push(newIntern);
-
-        lastQuest();
-
-
-    }).catch(err => console.log(err));
+      lastQuest();
+    })
+    .catch((err) => console.log(err));
 
 const engineerInput = () =>
-    prompt([
-        {
-            type: "input",
-            message: "What is your enginner's name?",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "What is your enginner's id?",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "What is your enginner's email?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "What is your enginner github username?",
-            name: "github",
-        },
+  prompt([
+    {
+      type: "input",
+      message: "What is your enginner's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your enginner's id?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your enginner's email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your enginner github username?",
+      name: "github",
+    },
+  ])
+    .then(({ name, id, email, github }) => {
+      const newEngineer = new Engineer(name, id, email, github);
+      team.push(newEngineer);
 
-    ]).then(({ name, id, email, github }) => {
-        const newEngineer = new Engineer(name, id, email, github);
-        team.push(newEngineer);
-
-        lastQuest();
-
-
-    }).catch(err => console.log(err));
-
+      lastQuest();
+    })
+    .catch((err) => console.log(err));
 
 const lastQuest = () =>
-    prompt(
-        {
-            type: "list",
-            message: "Wich type of team member would you like to add??",
-            name: "add",
-            choices: ["Intern", "Engineer", "I don't want to add more team members"],
-        },
-    )
-        .then((answers) => {
-            if (answers.add === "Intern") {
-                internInput();
-            } else if (answers.add === "Engineer") {
-                engineerInput();
-            }
-        })
-
-        .then(generateHtml);
+  prompt({
+    type: "list",
+    message: "Wich type of team member would you like to add??",
+    name: "add",
+    choices: ["Intern", "Engineer", "I don't want to add more team members"],
+  })
+    .then((answers) => {
+      if (answers.add === "Intern") {
+        internInput();
+      } else if (answers.add === "Engineer") {
+        engineerInput();
+      }
+    })
+    .then(generateHtml);
 
 // ----------------------------------------------------------------------------------
 const generateHtml = () => {
-    const html = `<!-- Instructor provided template -->
+  const html = `<!-- Instructor provided template -->
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -174,16 +166,16 @@ const generateHtml = () => {
         &copy; 2022-2023
       </footer>
     </body>
-    </html>  `
+    </html>  `;
 
-    writeToFile(html);
-}
+  writeToFile(html);
+};
 
 const generateEmployeeCards = () => {
-    const card = team.map(teamMember => {
-        const role = teamMember.getRole()
-        if (role === "Manager") {
-            return `<article>
+  const card = team.map((teamMember) => {
+    const role = teamMember.getRole();
+    if (role === "Manager") {
+      return `<article>
              <h2>Manager</h2>
              <ul>
              <li>ID: ${teamMember.name}</li>
@@ -191,11 +183,9 @@ const generateEmployeeCards = () => {
              <li>Email: ${teamMember.email}</li>
              <li>Office Number: ${teamMember.officeNumber}</li>
              </ul>
-             </article>`
-        }
-
-        else if (role === "Intern") {
-            return `<article>
+             </article>`;
+    } else if (role === "Intern") {
+      return `<article>
              <h2>Intern</h2>
              <ul>
              <li>ID: ${teamMember.name}</li>
@@ -203,11 +193,9 @@ const generateEmployeeCards = () => {
              <li>Email: ${teamMember.email}</li>
              <li>School: ${teamMember.school}</li>
              </ul>
-             </article>`
-        }
-
-        else if (role === "Engineer") {
-            return `<article>
+             </article>`;
+    } else if (role === "Engineer") {
+      return `<article>
              <h2>Engineer</h2>
              <ul>
              <li>ID: ${teamMember.name}</li>
@@ -215,14 +203,12 @@ const generateEmployeeCards = () => {
              <li>Email: ${teamMember.email}</li>
              <li>GitHub: <a href="https://github.com/${teamMember.githubUsername}">${teamMember.githubUsername}</a></a></li>
              </ul>
-             </article>`
-        }
-    })
-    return card.join("");
+             </article>`;
+    }
+  });
+  return card.join("");
 };
 
 const writeToFile = (html) => {
-
-    fs.writeFileSync('team.html', html);
-
+  fs.writeFileSync("team.html", html);
 };
